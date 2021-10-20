61d0d485fedd9369385d8f8bb1a88b9931ceffa5
Martin van Velsen <vvelsen@gmail.com>
2021-10-20 12:28:05 -0400

    A bunch of refactoring to make sure we're up-to-date with the latest react spects. Changed some styles around and cleaned up a lot of the css and code internals

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import Alert from './lib/components/Alert';
 
 import './App.css';
 
+/**
+ * 
+ */
 class App extends Component {
 
+  /**
+   * 
+   */
   constructor (props) {
     super (props);
 
     this.state = {
-      showAlert: "hidden",
-      showConfirm: "hidden",
+      show: "hidden",
       type: "alert"
     };
 
@@ -19,20 +24,31 @@ class App extends Component {
     this.onShowConfirm=this.onShowConfirm.bind(this);
   }
 
+  /**
+   * 
+   */
   onShowAlert () {
     console.log ("onShowAlert ()");
-    this.setState ({showAlert: "shown", type: "alert"});
+    this.setState ({show: "shown", type: "alert"});
   }
 
+  /**
+   * 
+   */
   onShowConfirm () {
     console.log ("onShowConfirm ()"); 
-    this.setState ({showConfirm: "shown", type: "confirm"});    
+    this.setState ({show: "shown", type: "confirm"});    
   }
 
+  /**
+   *
+   */
   render () {
+    console.log ("App: " + this.state.show);
+
     return (
       <div className="main">
-        <Alert type={this.state.type} state={this.state.showAlert} message="This is an windows.alert() replacement component. Please see the documentation for props and events"/>
+        <Alert type={this.state.type} state={this.state.show} message="This is an windows.alert() replacement component. Please see the documentation for props and events"/>
 
         <button className="demo-button" onClick={this.onShowAlert}>Show alert</button>
         <button className="demo-button" onClick={this.onShowConfirm}>Show confirm</button>
--- src/lib/components/Alert.js
+++ src/lib/components/Alert.js
@@ -14,9 +14,11 @@ class Alert extends Component {
     super(props);
 
     this.state = {
+      title: window.location.href + " says:",
       message: props.message,
-      state: props.state,
-      type: "alert"
+      state: "hidden",
+      type: props.type,
+      ownUpdate: false
     };
 
     this.onOk=this.onOk.bind(this);
@@ -25,12 +27,35 @@ class Alert extends Component {
   }
 
   /**
+   * Return null to indicate no change to state.
+   */
+  static getDerivedStateFromProps(nextProps, state) {
+    //console.log ("getDerivedStateFromProps ()");
+
+    if (!state.ownUpdate) {
+      if (state.state !== nextProps.state) {
+        return {
+          state: nextProps.state,
+          type: nextProps.type,
+          ownUpdate: false
+        };
+      }
+    } else {
+      return {
+        ownUpdate: false
+      }; 
+    }
+    
+    return null;
+  }  
+
+  /**
    *
    */
   onOk () {
     console.log ("onOk ()");
 
-    this.setState ({state: "hidden"});
+    this.setState ({state: "hidden", ownUpdate: true});
   }
 
   /**
@@ -39,7 +64,7 @@ class Alert extends Component {
   onYes () {
     console.log ("onYes ()");
 
-    this.setState ({state: "hidden"});
+    this.setState ({state: "hidden", ownUpdate: true});
   }
 
   /**
@@ -48,7 +73,7 @@ class Alert extends Component {
   onNo () {
     console.log ("onNo ()");
 
-    this.setState ({state: "hidden"});
+    this.setState ({state: "hidden", ownUpdate: true});
   }    
   
   /**
@@ -56,25 +81,27 @@ class Alert extends Component {
    */
   render () {
     let buttonBar;
-    let classes="eberly-alert";
+    let alertClass="eberly-alert";
+    let classes=alertClass;
 
     if (this.state.type==="alert") {
       buttonBar=<div className="eberly-alert-buttonbar"><button className="eberly-alert-button" onClick={this.onOk}>Ok</button></div>;
     } else {
       if (this.state.type==="confirm") {
-        buttonBar=<div className="eberly-alert-buttonbar"><button className="eberly-alert-button" onClick={this.onYes}>Ok</button><button onClick={this.onNo}>Cancel</button></div>;
+        buttonBar=<div className="eberly-alert-buttonbar"><button className="eberly-alert-button" onClick={this.onYes}>Ok</button><button className="eberly-alert-button" onClick={this.onNo}>Cancel</button></div>;
       } else {
         buttonBar=<div className="eberly-alert-buttonbar"><button className="eberly-alert-button" onClick={this.onOk}>Ok</button></div>;
       }
     }
 
     if (this.state.state==="shown") {
-      classes=classes + " " + "eberly-alert-shown";
+      classes=alertClass + " " + "eberly-alert-shown";
     } else {
-      classes=classes + " " + "eberly-alert-hidden";
+      classes=alertClass + " " + "eberly-alert-hidden";
     }
 
     return (<div className={classes}>
+        <div className="eberly-alert-title">{this.state.title}</div>
         <div className="eberly-alert-content">{this.state.message}</div>
         {buttonBar}
         </div>);
